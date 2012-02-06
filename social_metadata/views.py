f17b67019209870a673559b457fc175105b79d97
Tomasz Wysocki <tomasz@wysocki.info>
2012-02-06 13:31:30 +0100

    Add SocialDataMixin.
    Change-Id: Ib221ab18c759da7885664c58574217cc894f5892
    Reviewed-on: http://review.pozytywnie.pl:8080/2569
    Reviewed-by: Tomasz Wysocki <tomwys@gmail.com>
    Tested-by: Tomasz Wysocki <tomwys@gmail.com>

---
+++ social_metadata/views.py
@@ -0,0 +1,39 @@
+from django.conf import settings
+from django.contrib.sites.models import Site
+from django.views.generic.detail import BaseDetailView
+
+class SocialDataMixin(object):
+    def get_social_url(self):
+        if isinstance(self, BaseDetailView):
+            return self.object.get_absolute_url()
+
+    def get_social_title(self):
+        return None
+
+    def get_social_images(self):
+        return None
+
+    def get_social_description(self):
+        return None
+
+    def get_social_site_name(self):
+        if 'django.contrib.sites' in settings.INSTALLED_APPS:
+            try:
+                return Site.objects.get_current().name
+            except Site.DoesNotExist:
+                pass
+        return getattr(settings, 'SOCIAL_METADATA_SITE_NAME', None)
+
+    def get_social_metadata(self):
+        return {
+            'title': self.get_social_title(),
+            'images': self.get_social_images(),
+            'description': self.get_social_description(),
+            'url': self.get_social_url(),
+            'site_name': self.get_social_site_name(),
+        }
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(SocialDataMixin, self).get_context_data(*args, **kwargs)
+        context['social_metadata'] = self.get_social_metadata()
+        return context
